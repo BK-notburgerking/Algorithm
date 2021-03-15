6fdca6c70f87c79f375b0793d7687ea67719ce4d
BK <oij1234567@gmail.com>
2021-03-15 20:27:39 +0900

    210315

--- Baekjoon/10026_적록색약.py
+++ Baekjoon/10026_적록색약.py
@@ -2,45 +2,45 @@ import sys
 sys.setrecursionlimit(100000)
 
 def dfs(r, c, color, status):
-    if status == 'nomal':
-        nomal_visit[r][c] = 1
+    if status == 'normal':
+        normal_visit[r][c] = 1
         for i in range(4):
             nr = r + dr[i]
             nc = c + dc[i]
             if 0 <= nr < N and 0 <= nc < N:
-                if nomal_visit[nr][nc] == 0 and rgb[nr][nc] == color:
-                    dfs(nr, nc, color, 'nomal')
+                if normal_visit[nr][nc] == 0 and rgb[nr][nc] == color:
+                    dfs(nr, nc, color, 'normal')
 
-    if status == 'abnomal':
-        abnomal_visit[r][c] = 1
+    if status == 'abnormal':
+        abnormal_visit[r][c] = 1
         for i in range(4):
             nr = r + dr[i]
             nc = c + dc[i]
             if 0 <= nr < N and 0 <= nc < N:
                 if color == 'R' or color == 'G':
-                    if abnomal_visit[nr][nc] == 0 and (rgb[nr][nc] == 'R' or rgb[nr][nc] == 'G'):
-                        dfs(nr, nc, color, 'abnomal')
+                    if abnormal_visit[nr][nc] == 0 and (rgb[nr][nc] == 'R' or rgb[nr][nc] == 'G'):
+                        dfs(nr, nc, color, 'abnormal')
                 else:
-                    if abnomal_visit[nr][nc] == 0 and rgb[nr][nc] == color:
-                        dfs(nr, nc, color, 'abnomal')
+                    if abnormal_visit[nr][nc] == 0 and rgb[nr][nc] == color:
+                        dfs(nr, nc, color, 'abnormal')
 
 N = int(input())
 rgb = [input() for _ in range(N)]
 
-nomal_visit = [([0] * N) for _ in range(N)]
-abnomal_visit = [([0] * N) for _ in range(N)]
-nomal_cnt = abnomal_cnt = 0
+normal_visit = [([0] * N) for _ in range(N)]
+abnormal_visit = [([0] * N) for _ in range(N)]
+normal_cnt = abnormal_cnt = 0
 
 dr = [-1, 0, 1, 0]
 dc = [0, 1, 0, -1]
 
 for i in range(N):
     for j in range(N):
-        if nomal_visit[i][j] == 0:
-            dfs(i, j, rgb[i][j], 'nomal')
-            nomal_cnt += 1
-        if abnomal_visit[i][j] == 0:
-            dfs(i, j, rgb[i][j], 'abnomal')
-            abnomal_cnt += 1
+        if normal_visit[i][j] == 0:
+            dfs(i, j, rgb[i][j], 'normal')
+            normal_cnt += 1
+        if abnormal_visit[i][j] == 0:
+            dfs(i, j, rgb[i][j], 'abnormal')
+            abnormal_cnt += 1
 
-print(nomal_cnt, abnomal_cnt)
+print(normal_cnt, abnormal_cnt)
