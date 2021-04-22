2829da9525755b47c8f0692b796d7474b79dacb0
BK <oij1234567@gmail.com>
2021-04-22 23:13:38 +0900

    210422

--- Programmers/여행경로.py
+++ Programmers/여행경로.py
@@ -1,5 +1,5 @@
 def solution(tickets):
-    adj = {}
+    adj = {} #인접리스트
     for dep, arr in tickets:
         if adj.get(dep) == None:
             adj[dep] = [arr]
@@ -7,16 +7,19 @@ def solution(tickets):
             adj[dep] += [arr]
 
     for dep in adj.keys():
-        adj[dep].sort(reverse=True)
+        adj[dep].sort(reverse=True) #가장 위부터 꺼내서 쓸거라 오름차순
+        #가장 위에 사전순 제일 빠른게 올라옴
 
-    s = ["ICN"]
+    s = ["ICN"] # 인천에서 시작
     ans = []
     while s:
-        depart = s.pop()
+        depart = s.pop() #스택에서 꺼냄 -> 출발지
+        # 만약 출발지가 실제로 출발가능하고 (인접리스트에 키로 존재)
+        # 아직 사용안한 티켓이 있으면 (밸류의 길이가 양수)
         if depart in adj and len(adj[depart]) > 0:
-            s.append(depart)
-            s.append(adj[depart].pop())
-        else:
-            ans.append(depart)
+            s.append(depart) #다시 스택에 넣어주고
+            s.append(adj[depart].pop()) #가장 위에있는 티켓을 스택에 넣어줌
+        else: # 해당 출발지에서 출발하는 티켓이 없는 경우
+            ans.append(depart) # 바로 정답에 넣어줌
 
     return ans[::-1]
