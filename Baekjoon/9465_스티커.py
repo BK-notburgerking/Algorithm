dd8aea37e0ea1ac5728218225e03746b03a7a0cd
BK <oij1234567@gmail.com>
2021-07-22 00:11:27 +0900

    210721

--- Baekjoon/9465_스티커.py
+++ Baekjoon/9465_스티커.py
@@ -1,8 +1,15 @@
+import sys; input = sys.stdin.readline
+
 for _ in range(int(input())):
     n = int(input())
-    arr = [list(map(int, input().split())) for _ in range(2)]
-    memo = [arr[0][0], arr[0][1]]
+    arr1 = list(map(int, input().split()))
+    arr2 = list(map(int, input().split()))
+    arr1_max = arr1[0]
+    arr2_max = arr2[0]
 
-    for c in range(1, n):
-        for r in range(2):
-            print(arr[r][c])
+    for i in range(1, n):
+        tmp_arr1_max = max(arr1[i] + arr2_max, arr1_max)
+        arr2_max = max(arr2[i] + arr1_max, arr2_max)
+        arr1_max = tmp_arr1_max
+
+    print(max(arr1_max, arr2_max))
